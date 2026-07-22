5333ac2317b76daa4769d0bb5a789009a0c4555e
Sahil <sahilaus2020@gmail.com>
2026-07-22 21:15:08 +1000

    Phase 4: Spoke VNet with peering to hub

---
+++ spoke-network.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "spoke" {
+  name     = "rg-spoke"          # <- e.g. "rg-spoke"
+  location = "australiaeast"
+}
+
+resource "azurerm_virtual_network" "spoke" {
+  name                = "vnet-spoke"   # <- e.g. "vnet-spoke"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.spoke.location
+  resource_group_name = azurerm_resource_group.spoke.name
+}
+
+resource "azurerm_subnet" "workload" {
+  name                 = "10.1.0.0/24"   # <- e.g. "subnet-workload"
+  resource_group_name  = azurerm_resource_group.spoke.name
+  virtual_network_name = azurerm_virtual_network.spoke.name
+  address_prefixes     = ["10.1.0.0/24"]   # workload subnet, carved from 10.1.0.0/16
+}
