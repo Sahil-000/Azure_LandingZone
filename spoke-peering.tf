030e43dc26ca881097215d73f42419640400715e
Sahil <sahilaus2020@gmail.com>
2026-07-25 00:37:30 +1000

    Added Spoke images and Spoke peering code to the repo.

---
+++ spoke-peering.tf
@@ -0,0 +1,17 @@
+#Peer spoke to hub
+resource "azurerm_virtual_network_peering" "spoke_to_hub" {
+  name                      = "spoke-to-hub"
+  resource_group_name       = azurerm_resource_group.spoke.name
+  virtual_network_name      = azurerm_virtual_network.spoke.name
+  remote_virtual_network_id = azurerm_virtual_network.hub.id
+  allow_forwarded_traffic   = true
+}
+
+#Peer hub to spoke
+resource "azurerm_virtual_network_peering" "hub_to_spoke" {
+    name = "hub-to-spoke"
+    resource_group_name = azurerm_resource_group.hub.name
+    virtual_network_name = azurerm_virtual_network.hub.name
+    remote_virtual_network_id = azurerm_virtual_network.spoke.id
+    allow_forwarded_traffic   = true
+}
